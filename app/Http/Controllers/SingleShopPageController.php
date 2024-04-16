4dc5c012459a6fbe0c5a5e728fcebf5592eb49da
hagop <hagopbazarbasian@gmail.com>
2024-04-16 14:48:23 +0400

    sdsds

--- app/Http/Controllers/SingleShopPageController.php
+++ app/Http/Controllers/SingleShopPageController.php
@@ -6,12 +6,13 @@ use Illuminate\Http\Request;
 use App\Models\newproduct;
 use App\Models\photo;
 
-class SingleShopPageController extends Controller
+class SingleShopPageController extends Controller 
 {
      public function index($id){
         try {
             $newproduct = newproduct::with('photo')->findOrFail($id);
-            return view('single-shop-page',compact('newproduct'));
+            $newproductalls = newproduct::with('photo')->get();
+            return view('single-shop-page',compact('newproduct','newproductalls'));
         } catch (\Throwable $th) {
            return redirect()->back()->with('error' , 'Error');
         }
